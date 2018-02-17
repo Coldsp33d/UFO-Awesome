f4ad5561aa10835f10d5e0b688dd92c7693023c6
Shiva Deviah <shivakdeviah@gmail.com>
2018-02-16 23:59:31 -0800

    Dataloader utility

--- utils.py
+++ utils.py
@@ -1,28 +1,66 @@
 import json
 import pandas as pd
 
-def load_data(how: str = 'json') -> pd.DataFrame:
-    """ 
+# constants
+df_cols = [
+          'sighted_at', 
+          'reported_at', 
+          'location', 
+          'duration', 
+          'shape', 
+          'description',
+        ]
+df_date_cols = ['sighted_at', 'reported_at']
+
+
+def load_data() -> pd.DataFrame:
+    ''' 
     Load data from Data/* into a pandas DataFrame
 
     Parameters
     ----------  
     how : specify the type of file to parse.
-        'json' loads from the JSON file, or 'tsv' to load from the TSV
-    """
+        'json' loads from the JSON file; 'tsv' loads from the TSV file
 
-    if how == 'json':
-        data = []
-        with open('Data/ufo_awesome.json') as f:
-            for line in f:
-                try:
-                    data.append(json.loads(line))
-                except ValueError:
-                    pass        # TODO fix buggy data
+    '''
+    try:  # attempt load with pd.read_json
+        df = _simple_json_loader()
+    except ValueError:
+          # fallback to loopy implementation reader (filters out corrupt data)
+        df = _default_json_loader()
 
-        return pd.DataFrame.from_records(data)
+    df[df_date_cols] = df[df_date_cols]\
+                       .astype(str)\
+                       .apply(
+                            pd.to_datetime, errors='coerce', format='%Y%m%d'
+                       )
 
-    # TODO implement how='tsv'
+    return df.reindex(columns=df_cols).replace('', pd.np.nan)
+
+
+def _simple_json_loader() -> pd.DataFrame:
+    '''
+    Can be used once ufo_awesome.json has been corrected. 
+    To fix the data, run `python3.6 fix_json_data.py`
+
+    ''' 
+    return pd.read_json('Data/ufo_awesome.json', lines=True)
+
+def _default_json_loader() -> pd.DataFrame:
+    '''
+    Default imlementation, use when ufo_awesome.json contains malformed data.
+
+    ''' 
+    data = []
+    with open('Data/ufo_awesome.json') as f:
+        for line in f:
+            try:
+                data.append(json.loads(line))
+            except ValueError:
+                pass        # TODO fix buggy data
+
+    return pd.DataFrame.from_records(data)
+
 
 if __name__ == '__main__':
     df = load_data()
