f635c8b21ee469fb7f0005e8099ded1aa9981eb8
Shiva Deviah <shivakdeviah@gmail.com>
2018-02-26 15:39:46 -0800

    Data join code

--- datajoiner.py
+++ datajoiner.py
@@ -1,18 +1,22 @@
 import utils
+import pandas as pd
 
-# load UFO sightings data
-df = utils.simple_csv_loader('Data/ufo_awesome_modified.csv', parse_dates=utils.df_date_cols)
-# load census data
+# load data
+df = utils.simple_csv_loader(
+            'Data/ufo_awesome_modified.csv', 
+            parse_dates=utils.df_date_cols
+)
+df_airport = utils.simple_csv_loader('Data/airport_codes_clean.csv')
+
 df_census = utils.simple_csv_loader('Data/census_clean.csv')
 
 #  ---- merge UFO with census data --- # 
-df['census_year'] = np.ceil(df.sighted_at.dt.year / 10) * 10  # surrogate column for joining on census year
+# surrogate column for joining on census year
+df['census_year'] = pd.np.ceil(df.sighted_at.fillna(df.reported_at).dt.year / 10) * 10  
 
 df = df.merge(
     df_census, on=['municipality', 'state', 'census_year'], how='left'
 )
 
-print(df.shape)
-print(df.dropna(subset=['municipality', 'state', 'census_year']).shape)
-
-
+#  ---- merge UFO with airport data --- # 
+df = df.merge(df_airport, on='municipality', how='left')
