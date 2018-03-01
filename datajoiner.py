eb0afe23deefaca1ebcba885e7d67eb61761250c
Shiva Deviah <shivakdeviah@gmail.com>
2018-02-28 16:08:06 -0800

    Completed join logic on ALL cleaned data

--- datajoiner.py
+++ datajoiner.py
@@ -1,26 +1,50 @@
-import utils
-
 import pandas as pd
+import os
 
-# ---- data loading --- #
-# load ufo data
-df = utils.simple_csv_loader(
-            'Data/ufo_awesome_modified.csv', 
-            parse_dates=utils.df_date_cols,
-            compression='gzip'
-)
-# load aiport data
-df_airport = utils.simple_csv_loader('Data/airport_clean.csv')
-# load combined census and geodata
-df_census = utils.simple_csv_loader('Data/census_clean.csv')
-# separate geodata from census data for easier management
-df_geodata = df_census[['municipality', 'state', 'zipcode', 'lat', 'lng']].drop_duplicates()
-df_census = df_census.drop(['zipcode', 'lat', 'lng'], 1)
-# load climate data
-df_climate = utils.simple_csv_loader('Data/climate_clean.csv')
+import utils
+import datacleaner
 
+#  ---- DATA LOADING ---- #
+datasetspec = {
+    'UFO'     : (
+                    'Data/ufo_awesome_modified.csv', 
+                    datacleaner.clean_ufo_data, 
+                    {
+                        'parse_dates' : utils.df_date_cols, 
+                        'compression' : 'gzip'
+
+                }),
+    'airport' : ('Data/airport_clean.csv', datacleaner.clean_airport_data, {}),
+    'census'  : ('Data/census_clean.csv' , datacleaner.clean_census_data , {}),
+    'climate' : ('Data/climate_clean.csv', datacleaner.clean_climate_data, {})
+}
+
+dfs = {}
+for dataset, (filepath, f, kwargs) in datasetspec.items():
+    print('Loading {} data...\t'.format(dataset), end='\r')
+
+    if not os.path.isfile(filepath):
+        dfs[dataset] = f(filepath)
+    else:
+        dfs[dataset] = utils.simple_csv_loader(filepath, **kwargs)
+
+    print('Loading {} data...\tDONE'.format(dataset))
+
+
+df, df_airport, df_census, df_climate = dfs['UFO'], dfs['airport'], dfs['census'], dfs['climate']
+
+#  ---- DATA JOINING --- 
+#
 #  ---- merge UFO with airport data --- # 
 df = df.merge(df_airport, on=['municipality', 'state'], how='left')
+
+#  ---- merge UFO with census data --- # 
+# surrogate column for joining on census year
+df['census_year'] = pd.np.ceil(df.sighted_at.fillna(df.reported_at).dt.year / 10) * 10  
+
+df = df.merge(
+   df_census, on=['municipality', 'state', 'census_year'], how='left'
+)
 
 # --- merge UFO with climate data --- #
 df = df.assign(
@@ -35,15 +59,6 @@ df = df.assign(
          ['temp', 'date'],             # cleanup unnecessary columns
          axis=1
      )
-
-
-#  ---- merge UFO with census data --- # 
-# surrogate column for joining on census year
-df['census_year'] = pd.np.ceil(df.sighted_at.fillna(df.reported_at).dt.year / 10) * 10  
-
-df = df.merge(
-    df_census, on=['municipality', 'state', 'census_year'], how='left'
-)
 
 print(df.shape)
 
